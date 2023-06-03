97f6294305d24262972f0240f91da035bda8deb7
ErikROliveira <erikrdeoliveira@hotmail.com>
2023-06-02 22:59:54 -0300

    Feito Ex.2.js

---
+++ Erik/Ex.2.js
@@ -0,0 +1,8 @@
+const N = 8;
+
+
+    if ( N % 2 == 0) 
+        console.log (" O numero que voce digitou é Par: " + N);
+    else
+        console.log (" O Numero que voce digitou é Impar: " + N);
+    
